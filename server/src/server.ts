e0276c7a33590a1f76ffec884c916dd7ba10cd4a
Anthony Schwab <schwab.it@pm.me>
2025-02-15 10:24:37 -0600

    updated server.ts to include sequelize init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
-// const forceDatabaseRefresh = false;
+const forceDatabaseRefresh = false;
 
 import express from 'express';
-// import sequelize from './config/connections.js';
+import sequelize from './config/connections.js';
 import routes from './routes/index.js';
 
 const app = express();
@@ -12,13 +12,13 @@ app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
 
-// sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server is listening on port ${PORT}`)
-//     });
-// });
+sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`)
+    });
+});
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT} `)
-} 
-)
+//app.listen(PORT, () => {
+//    console.log(`Server is listening on port ${PORT} `)
+//}
+//)
