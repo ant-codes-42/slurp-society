9c120326f5398b860b1a222b47520b7b4aa727fe
Anthony Schwab <schwab.it@pm.me>
2025-02-18 13:07:50 -0600

    refactored reservation form to pull data based on login

--- client/src/components/ReservationForm.tsx
+++ client/src/components/ReservationForm.tsx
@@ -1,58 +1,102 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getUserById } from '../api/userAPI';
+import type { UserData } from '../interfaces/UserData';
+import auth from '../utils/Auth';
 
-export default function ReservationForm({slot, incomingPartySize}: {slot: string, incomingPartySize: string}) {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
+export default function ReservationForm({ slot, incomingPartySize }: { slot: string, incomingPartySize: string }) {
+    const [userObj, setUserObj] = useState<UserData>({
+        id: null,
+        email: null,
+        name: null,
+        phone: null,
+        password: null,
+    });
+    const [specialRequests, setSpecialRequests] = useState('');
     const [partySize, setPartySize] = useState(incomingPartySize);
     const slotId = slot;
     console.log(slotId); //temp logging
+
+    const [loginCheck, setLoginCheck] = useState(false);
+
+    const checkLogin = () => {
+        if (auth.loggedIn()) {
+            setLoginCheck(true);
+        }
+    };
+
+    useEffect(() => {
+        checkLogin();
+    }, []);
+
+    useEffect(() => {
+        if (loginCheck) {
+            //Add decode token and API call to get user data
+            setUserObj(prevState => ({
+                ...prevState,
+                id: auth.getProfile().id
+            }));
+        }
+    }, [loginCheck]);
+
+    useEffect(() => {
+        if (userObj.id) {
+            getUserById(userObj.id)
+                .then((user) => {
+                    console.log('User:', user);
+                })
+                .catch((error) => {
+                    console.error('Error fetching user:', error);
+                });
+        }
+    }, [userObj.id])
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { target } = e;
         const inputType = target.name;
         const inputValue = target.value;
 
-        if (inputType === 'name') {
-            setName(inputValue);
-        } else if (inputType === 'email') {
-            setEmail(inputValue);
-        } else if (inputType === 'phone') {
-            setPhone(inputValue);
+        if (inputType === 'specialRequests') {
+            setSpecialRequests(inputValue);
         } else if (inputType === 'partySize') {
             setPartySize(inputValue);
         }
     };
 
+
+
     const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
-        if (!name || !email || !phone || !partySize) {
-            alert('Please fill out all fields');
-            return;
-        }
 
         //Add API call to create reservation
     };
 
     return (
-        <div>
-        <h1>Reservation Form</h1>
-        <form onSubmit={handleFormSubmit}>
-            <div>
-            <label htmlFor="name">Name</label>
-            <input type="text" value={name} onChange={handleInputChange} id="name" name="name" />
-            <label htmlFor="email">Email</label>
-            <input type="email" value={email} onChange={handleInputChange} id="email" name="email" />
-            <label htmlFor="phone">Phone</label>
-            <input type="phone" value={phone} onChange={handleInputChange} id="phone" name="phone" />
-            <label htmlFor="partySize">Party Size</label>
-            <input type="number" value={partySize} onChange={handleInputChange} id="partySize" name="partySize" defaultValue={incomingPartySize} />
-            </div>
-            <div>
-                <input type="submit" value="Submit" />
-            </div>
-        </form>
-        </div>
+        <>
+            {!loginCheck ? (
+                <div>
+                    <h1>Login to make a reservation</h1>
+                </div>
+            ) : (
+                <div>
+                    <h1>Reservation Form</h1>
+                    <form onSubmit={handleFormSubmit}>
+                        <div>
+                            <label htmlFor="name">Name</label>
+                            <input type="text" value={userObj.name ? userObj.name : ''} id="name" name="name" />
+                            <label htmlFor="email">Email</label>
+                            <input type="email" value={userObj.email ? userObj.email : ''} id="email" name="email" />
+                            <label htmlFor="phone">Phone</label>
+                            <input type="phone" value={userObj.phone ? userObj.phone : ''} id="phone" name="phone" />
+                            <label htmlFor="partySize">Party Size</label>
+                            <input type="specialRequests" value={specialRequests} onChange={handleInputChange} id="specialRequests" name="specialRequests" />
+                            <input type="number" value={partySize} onChange={handleInputChange} id="partySize" name="partySize" defaultValue={incomingPartySize} min={1} />
+                        </div>
+                        <div>
+                            <input type="submit" value="Submit" />
+                        </div>
+                    </form>
+                </div>
+            )}
+        </>
     );
 }
