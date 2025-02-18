a9739e5498c6e6c930eb5fa3a1fdbefd5f5158d7
Tristan Persaud <tristanppersaud@gmail.com>
2025-02-18 07:23:03 -0600

    commit to save

---
+++ client/src/pages/Contact.tsx
@@ -0,0 +1,26 @@
+const Contact = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    Anthony Schwab
+                    Github: ant-codes-42
+                </li>
+
+                <li>
+                    Vincent Thao
+                    Github: vincent
+                </li>
+
+                <li>
+                    Lindsey Vigeesa
+                    Github:
+                </li>
+                <li>
+                    Tristan Persaud
+                    Github: 
+                </li>
+            </ul>
+        </div>
+    )
+}
