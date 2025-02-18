06d75aa75f095428e0915f0be43f127f0ae4e085
Tristan Persaud <tristanppersaud@gmail.com>
2025-02-18 07:38:12 -0600

    completed-contact-page

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,26 +1,34 @@
 const Contact = () => {
     return (
-        <div>
-            <ul>
-                <li>
+        <>
+        <div className="contactH2">
+            <h2>If you have any questions or concerns, please reach out to us at:</h2>
+        </div>
+        
+        <div className="contactList">
+            <ul id="contactUl" className="contactUL">
+                <li className="contactLi">
                     Anthony Schwab
-                    Github: ant-codes-42
+                    <a href="https://github.com/ant-codes-42">Github: ant-codes-42</a>
                 </li>
 
                 <li>
                     Vincent Thao
-                    Github: vincent
+                    <a href="https://github.com/vincentt94">Github: vincentt94</a>
                 </li>
 
                 <li>
-                    Lindsey Vigeesa
-                    Github:
+                    Lindsey Vigeesa  
+                    <a href="https://github.com/lindsey078">Github: lindsey078</a>
                 </li>
                 <li>
                     Tristan Persaud
-                    Github: 
+                    <a href="https://github.com/TristanPPersaud">Github: TristanPPersaud</a>
                 </li>
             </ul>
         </div>
+        </>
     )
 }
+
+export default Contact 
